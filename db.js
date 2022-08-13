b250d8b664d222391ba5c3648832622df3ce15a5
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-13 22:24:49 +0500

    getting starting with express.js

--- db.js
+++ db.js
@@ -2,8 +2,20 @@ const mongoose = require("mongoose");
 const mongooseURI = "mongodb://localhost:27017/";
 const connectToMongo = () => {
   mongoose.connect(mongooseURI, () => {
-    console.log("V");
+    console.log("Connected");
   });
 };
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Abd!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 module.exports = connectToMongo;
