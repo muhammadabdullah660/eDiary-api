deefd7210282fb30cb905c80b38a59cc1d7ce4b6
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-14 23:58:30 +0500

    adding validations

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const User = require("../Models/User");
 const router = express.Router();
-
+const { body, validationResult } = require("express-validator");
 //Create a user using : POST "/api/auth"
-router.post("/", (req, res) => {
+router.post("/", [], (req, res) => {
+  body("userName").isLength({ min: 3 });
+  body("email").isEmail();
+  body("password").isLength({ min: 8 });
   console.log(req.body);
   const user = User(req.body);
   user.save();
