537c54ce338bb0133ef48016193c9b2a02322a1f
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-14 23:59:18 +0500

    handling errors

--- Routes/auth.js
+++ Routes/auth.js
@@ -7,9 +7,10 @@ router.post("/", [], (req, res) => {
   body("userName").isLength({ min: 3 });
   body("email").isEmail();
   body("password").isLength({ min: 8 });
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   res.send("Hello!!");
 });
 module.exports = router;
