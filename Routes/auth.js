ba526476b2e8c165000e1ad16075075925a37cbe
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-15 00:03:33 +0500

    error: validations not in array, status: fixed

--- Routes/auth.js
+++ Routes/auth.js
@@ -3,14 +3,19 @@ const User = require("../Models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 //Create a user using : POST "/api/auth"
-router.post("/", [], (req, res) => {
-  body("userName").isLength({ min: 3 });
-  body("email").isEmail();
-  body("password").isLength({ min: 8 });
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+router.post(
+  "/",
+  [
+    body("userName").isLength({ min: 3 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 8 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("Hello!!");
   }
-  res.send("Hello!!");
-});
+);
 module.exports = router;
